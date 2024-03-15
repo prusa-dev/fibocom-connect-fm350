2c91bd6d7d377920c0dbdbc4800e2a9436e8978d
prusa-dev <prusa@mail.ru>
2024-03-15 19:13:06 +0300

    fix: 24h format in log file name

--- connect_with_logs.cmd
+++ connect_with_logs.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "cd '%~dp0%scripts'; & ./main.ps1 -Verbose 4>../log-$((Get-Date).ToString('yyyyMMddhhmmss')).txt"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "cd '%~dp0%scripts'; & ./main.ps1 -Verbose 4>../log-$((Get-Date).ToString('yyyyMMddHHmmss')).txt"
 
 if ERRORLEVEL 1 pause
