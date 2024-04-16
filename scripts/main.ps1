3b7b653def1dda8b6239dc799b334d782df65885
prusa-dev <prusa@mail.ru>
2024-04-16 16:50:33 +0300

    bump version

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -7,7 +7,7 @@ param(
 $PSDefaultParameterValues = @{"*:Verbose" = ($VerbosePreference -eq 'Continue') }
 $ErrorActionPreference = 'Stop'
 
-$app_version = "Fibocom Connect FM350 v2024.04.2"
+$app_version = "Fibocom Connect FM350 v2024.04.3"
 
 Clear-Host
 
